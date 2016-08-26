c8e97cc5741627efc7a3e6baa4c399df38415221
hardvic <hardvic@outlook.com>
2016-08-26 18:28:36 +0800

    在 polls 中添加 tasks.py

--- djcelery_proj/settings.py
+++ djcelery_proj/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'kombu.transport.django',
+    # 'kombu.transport.django',
     'djcelery',  # 添加 djcelery
     'polls',
 ]
--- polls/tasks.py
+++ polls/tasks.py
@@ -2,7 +2,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from celery import shared_task
+from celery import task
 # from djcelery_pro.celery import app
+
+
+@task
+def add_1(x, y):
+    return x + y
+
+
+@shared_task
+def add_2(x, y):
+    return x + y
 
 
 @shared_task
