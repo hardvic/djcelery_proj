0ce9fcf0a091eced316861a7e968c2454ede888f
hardvic <hardvic@outlook.com>
2016-08-26 17:11:42 +0800

    在 polls 中添加 tasks.py

--- djcelery_proj/settings.py
+++ djcelery_proj/settings.py
@@ -50,8 +50,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'kombu.transport.django',
     'djcelery',  # 添加 djcelery
-
+    'polls',
 ]
 
 MIDDLEWARE_CLASSES = [
